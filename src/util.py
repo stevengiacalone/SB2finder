43cbe747cceb39de26ceb1731155955452b397f5
willsalazar5221 <124869416+willsalazar5221@users.noreply.github.com>
2024-07-08 10:37:28 -0700

    Added functions to util.py

--- src/util.py
+++ src/util.py
@@ -2,4 +2,60 @@ import numpy as np
 import pandas as pd
 import scipy.interpolate as inter 
 from astropy.io import fits
+from astropy.table import Table
 
+def spline_fit(x,y,window):
+    breakpoint = np.linspace(np.min(x),np.max(x),int((np.max(x)-np.min(x))/window))
+    ss = inter.LSQUnivariateSpline(x,y,breakpoint[1:-1])
+    return ss
+
+def flatspec_spline(x,rawspec,weight, order=8, n_iter = 5, ffrac = 0.985):
+
+    pos = np.where((np.isnan(rawspec)==False) & (np.isnan(weight)==False))[0]
+
+
+    ss = spline_fit(x[pos],rawspec[pos],5.)#5 Ang as knot points for flattening continuum
+    yfit = ss(x)
+
+    for i in range(n_iter):
+        normspec = rawspec / yfit
+        pos = np.where((normspec >= ffrac) & (yfit > 0))[0]#& (normspec <= 2.)
+
+        ss = spline_fit(x[pos],rawspec[pos],5.)
+        yfit = ss(x)
+    normspec = rawspec / yfit
+
+    return normspec,yfit
+
+
+def get_spectrum(file_name, trace_num, color):
+    L1_file = file_name
+    L1 = fits.open(file_name)
+    
+    if color == 'GREEN'
+        if trace_num == 1:
+            SCI_WAVE = np.array(L1['GREEN_SCI_WAVE1'].data)
+            SCI_FLUX = np.array(L1['GREEN_SCI_FLUX1'].data)
+        elif trace_num == 2:
+            SCI_WAVE = np.array(L1['GREEN_SCI_WAVE2'].data)
+            SCI_FLUX = np.array(L1['GREEN_SCI_FLUX2'].data)
+        else:
+            SCI_WAVE = np.array(L1['GREEN_SCI_WAVE3'].data)
+            SCI_FLUX = np.array(L1['GREEN_SCI_FLUX3'].data)    
+    else:
+        if trace_num == 1:
+            SCI_WAVE = np.array(L1['RED_SCI_WAVE1'].data)
+            SCI_FLUX = np.array(L1['RED_SCI_FLUX1'].data)
+        elif trace_num == 2:
+            SCI_WAVE = np.array(L1['RED_SCI_WAVE2'].data)
+            SCI_FLUX = np.array(L1['RED_SCI_FLUX2'].data)
+        else:
+            SCI_WAVE = np.array(L1['RED_SCI_WAVE3'].data)
+            SCI_FLUX = np.array(L1['RED_SCI_FLUX3'].data)
+            
+    return SCI_WAVE, SCI_FLUX
+    
+
+    
+    
+    
